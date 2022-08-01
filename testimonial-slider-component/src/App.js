a386a311dae88ab11f36836a035c3be3ec3cba08
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-01 13:19:35 -0400

    Add app component and mobile view css

--- testimonial-slider-component/src/App.js
+++ testimonial-slider-component/src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <section id="image-container">
+        <img className="testimonial-img" src="images/image-tanya.jpg" alt="Testimonial Profile"/>
+        <div className="btn-container">
+          <img id="previous" className="btn" src="images/icon-prev.svg" alt="Previous Button"/>
+          <img id="next" className="btn" src="images/icon-next.svg" alt="Next Button"/>
+        </div>
+      </section>
+      <section id="testimonial-container">
+        <p>
+          “ I’ve been interested in coding for a while but never taken the jump, until now. 
+          I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+          excited about the future. ”
+        </p>
+        <span className="name">Tanya Sinclair</span>
+        <span className="title">UX Designer</span>
+      </section>
     </div>
   );
 }
