d164624e180a4dfa1191746d4517f2d40687a529
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-01 21:19:19 -0400

    Add desktop css and react state

--- testimonial-slider-component/src/App.js
+++ testimonial-slider-component/src/App.js
@@ -1,24 +1,35 @@
+import React, {useState} from "react"
 import './App.css';
+import users from "./profiles"
 
 function App() {
+
+  const [profile, setProfile] = useState(true)
+  const user = users;
+
+
+  const handleClick = () =>{
+    setProfile(!profile)
+  }
+
   return (
     <div className="App">
-      <section id="image-container">
-        <img className="testimonial-img" src="images/image-tanya.jpg" alt="Testimonial Profile"/>
-        <div className="btn-container">
-          <img id="previous" className="btn" src="images/icon-prev.svg" alt="Previous Button"/>
-          <img id="next" className="btn" src="images/icon-next.svg" alt="Next Button"/>
-        </div>
-      </section>
-      <section id="testimonial-container">
-        <p>
-          “ I’ve been interested in coding for a while but never taken the jump, until now. 
-          I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-          excited about the future. ”
-        </p>
-        <span className="name">Tanya Sinclair</span>
-        <span className="title">UX Designer</span>
-      </section>
+      <div className="site-container">
+        <section id="image-container">
+          <img className="testimonial-img" src={profile ? user[0].photo : user[1].photo} alt="Testimonial Profile"/>
+          <div className="btn-container">
+            <img onClick={handleClick} id="previous" className="btn" src="images/icon-prev.svg" alt="Previous Button"/>
+            <img onClick={handleClick} id="next" className="btn" src="images/icon-next.svg" alt="Next Button"/>
+          </div>
+        </section>
+        <section id="testimonial-container">
+          <p className="testimonial-text">{profile ? user[0].testimonial : user[1].testimonial}</p>
+          <div>
+            <span className="name">{profile ? user[0].name : user[1].name}</span>
+            <span className="title">{profile ? user[0].title : user[1].title}</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ testimonial-slider-component/src/profiles.js
@@ -0,0 +1,17 @@
+const users=[
+    {
+      photo:"images/image-tanya.jpg",
+      name:"Tanya Sinclair",
+      title:"UX Engineer",
+      testimonial:"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."
+    },
+
+    {
+      photo:"images/image-john.jpg",
+      name:"John Tarkpor",
+      title:"Junior Front-end Developer",
+      testimonial:"If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."
+    }
+  ];
+
+  export default users;
